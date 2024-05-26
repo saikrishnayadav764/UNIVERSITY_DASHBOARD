0d149c370b4a32991f1c95d728c4828caf48ee86
saikrishnayadav764 <106056182+saikrishnayadav764@users.noreply.github.com>
2024-05-26 19:31:07 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 const api = axios.create({
-  baseURL: 'https://university-backend-gu9a.onrender.com/api',
+  baseURL: 'https://university-backend-x31v.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
